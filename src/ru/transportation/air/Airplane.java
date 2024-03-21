f1379c89586ae9b0f2fd4f1c15ab55c924e05dfa
HappyBorer <BorerKicker@yandex.ru>
2024-03-21 08:03:37 +0300

    created class Truck, Train, Tanker and Airplane

---
+++ src/ru/transportation/air/Airplane.java
@@ -0,0 +1,7 @@
+package ru.transportation.air;
+
+import ru.transportation.PassengerTransportation;
+import ru.transportation.Transportation;
+
+public class Airbus extends Transportation implements PassengerTransportation {
+}
---
+++ src/ru/transportation/water/Tanker.java
@@ -0,0 +1,2 @@
+package ru.transportation.water;public class Tanker {
+}
